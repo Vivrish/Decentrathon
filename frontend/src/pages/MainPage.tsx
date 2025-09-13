401140173152af0337db1d5697d76f4382bf052c
howardat <olzhas.nurzhanovich.kakimzhanov@gmail.com>
2025-09-13 21:09:34 +0500

    Create main page v2

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,18 +1,58 @@
 import { Button } from "../components/ui/button";
 
+// async function uploadImages(files: File[]) {
+//   const res = await evaluateCar(files);
+//   console.log(res);
+// }
+
 export default function MainPage() {
   return (
-    <>
-      <div className="flex items-center flex-col">
-        <div className="flex min-w-[370px] flex-col items-center justify-center bg-red-100">
-          <p>Решение команды ExeQtion</p>
-          <h1>
-            Оцените состояние <mark className="bg-amber-100">автомобиля</mark>
+    <div className="flex flex-col items-center space-between w-full h-lvh py-6">
+      <div className="flex flex-col gap-2">
+        <img src="src/assets/logos.png" alt="" className="w-55" />
+        <p className="leading-5 text-sm text-neutral-500 text-center">
+          Решение команды ExeQtion
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-center grow gap-6">
+        <div className="flex felx-col gap-2">
+          <img
+            className="w-18 h-24 bg-neutral-50 rounded-md"
+            src="src/assets/images/main/Frame 6.png"
+            alt=""
+          />
+          <img
+            className="w-18 h-24 bg-neutral-50 rounded-md"
+            src="src/assets/images/main/Frame 7.png"
+            alt=""
+          />
+          <img
+            className="w-18 h-24 bg-neutral-50 rounded-md"
+            src="src/assets/images/main/Frame 8.png"
+            alt=""
+          />
+          <img
+            className="w-18 h-24 bg-neutral-50 rounded-md"
+            src="src/assets/images/main/Frame 9.png"
+            alt=""
+          />
+        </div>
+        <div className="flex flex-col gap-5 items-center">
+          <h1 className="text-center text-4xl font-extrabold tracking-tight text-balanced">
+            Оцените состояние <mark className="highlight">автомобиля</mark>
           </h1>
-          <p>Загрузите фотографий автомобиля и получите оценку безопасности</p>
-          <Button>Оценить состояние</Button>
+          <p className="leading-5 text-neutral-500 max-w-70  text-center">
+            Загрузите фотографий автомобиля и получите оценку безопасности
+          </p>
+          <Button className="max-w-90 w-full">Попробовать решение</Button>
         </div>
       </div>
-    </>
+      {/* <div>
+        <Button className="bg-accent">Попробовать решение</Button>
+      </div> */}
+      {/* <div className="flex min-h-svh flex-col items-center justify-center">
+        <MultiImageUpload onUpload={uploadImages} maxFiles={5} /> */}
+      {/* </div> */}
+    </div>
   );
 }
