28c01a4f2b74cfbac5d8833e7792c4b0a6a09a18
Nikolai Chaunin <chaunnik@fit.cvut.cz>
2025-09-13 11:24:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Application for a hackathon - detects car state
+
+# Run
+
+1. install docker
+2. docker compose up
+
+# Development
+
+Backend:
+
+1. Create a venv for python, activate it
+2. pip install -r requirements.txt
---
+++ backend/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import io
+from PIL import Image
+from enum import Enum
+
+class CarState(Enum):
+    Clean = 0
+    Dirty = 1
+
+
+app = Flask(__name__)
+
+def evaluateCar(image: Image.Image) -> CarState:
+    return CarState.Clean # TODO needs to be implemented by ML guys
+
+
+@app.route('/evaluate', methods=['GET'])
+def evaluate():
+    file = request.files['carImage'].read()
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
