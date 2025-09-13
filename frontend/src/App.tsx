a565a575669b5bc48a3d3c6899a5542a2aa25f16
howardat <olzhas.nurzhanovich.kakimzhanov@gmail.com>
2025-09-13 19:50:25 +0500

    Created main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { evaluateCar } from "@/api/evaluateCar";
 import "./App.css";
 import { MultiImageUpload } from "./components/multiImageUpload";
+import { Button } from "./components/ui/button";
 
 async function uploadImages(files: File[]) {
   const res = await evaluateCar(files);
@@ -12,6 +13,17 @@ function App() {
     <>
       <div className="flex min-h-svh flex-col items-center justify-center">
         <MultiImageUpload onUpload={uploadImages} maxFiles={5} />
+      </div>
+
+      <div className="flex items-center flex-col">
+        <div className="flex min-w-[370px] flex-col items-center justify-center bg-red-100">
+          <p>Решение команды ExeQtion</p>
+          <h1>
+            Оцените состояние <mark className="bg-amber-100">автомобиля</mark>
+          </h1>
+          <p>Загрузите фотографий автомобиля и получите оценку безопасности</p>
+          <Button>Оценить состояние</Button>
+        </div>
       </div>
     </>
   );
